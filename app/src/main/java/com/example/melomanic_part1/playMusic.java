47a6fd99c19124a648cad9591b46712331a9ee9c
Vadim-tetotovich <83085936+Vadim-tetotovich@users.noreply.github.com>
2022-04-30 22:06:45 +0300

    аааааа наконец-то добавил эту долбанную музыку

--- app/src/main/java/com/example/melomanic_part1/playMusic.java
+++ app/src/main/java/com/example/melomanic_part1/playMusic.java
@@ -2,16 +2,30 @@ package com.example.melomanic_part1;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class playMusic extends AppCompatActivity {
 
     public static final String APP_PREFERENCES = "FilePreferences";
+    String url = "https://api.jsonbin.io/b/626d79c025069545a32b468d/2";
 
     TextView title;
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,30 @@ public class playMusic extends AppCompatActivity {
         title = findViewById(R.id.musicGenreTitle);
         title.setText(titleGenre);
 
+        RequestQueue mQueue = Volley.newRequestQueue(this);
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("music");
+                            JSONObject empl = jsonArray.getJSONObject(0);
+                            String name = empl.getString("src");
+//                            Uri uri = Uri.parse(name);
+                            mPlayer = MediaPlayer.create(getApplicationContext(),getResources().getIdentifier(name, "raw", getPackageName()));
+                            mPlayer.start();
+//                            title.setText(name);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 }
