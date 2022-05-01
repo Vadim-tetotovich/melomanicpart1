d7fdf3b309537e240a4a989ebcba2dba9f01a9fe
Vadim-tetotovich <83085936+Vadim-tetotovich@users.noreply.github.com>
2022-05-01 13:05:42 +0300

    add rock playlist

--- app/src/main/java/com/example/melomanic_part1/playMusic.java
+++ app/src/main/java/com/example/melomanic_part1/playMusic.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,24 +20,35 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Random;
+
 public class playMusic extends AppCompatActivity {
 
     public static final String APP_PREFERENCES = "FilePreferences";
-    String url = "https://api.jsonbin.io/b/626d79c025069545a32b468d/2";
+    String url = "https://api.jsonbin.io/b/626d79c025069545a32b468d/5";
 
     TextView title;
     MediaPlayer mPlayer;
+    Button chooseBtn1, chooseBtn2, chooseBtn3, chooseBtn4;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
 
+        random = new Random();
+
         SharedPreferences sharedPreferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
 
         String titleGenre = sharedPreferences.getString("thisGenre", "unknown");
 
         title = findViewById(R.id.musicGenreTitle);
+        chooseBtn1 = findViewById(R.id.chooseBtn1);
+        chooseBtn2 = findViewById(R.id.chooseBtn2);
+        chooseBtn3 = findViewById(R.id.chooseBtn3);
+        chooseBtn4 = findViewById(R.id.chooseBtn4);
+
         title.setText(titleGenre);
 
         RequestQueue mQueue = Volley.newRequestQueue(this);
@@ -46,12 +58,17 @@ public class playMusic extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONArray jsonArray = response.getJSONArray("music");
-                            JSONObject musics = jsonArray.getJSONObject(0);
-                            String src = musics.getString("src");
+                            JSONObject playlist = response.getJSONObject(titleGenre);
+                            JSONArray musics = playlist.getJSONArray("music");
+                            JSONObject music = musics.getJSONObject(random.nextInt(musics.length()));
+                            String src = music.getString("src");
+                            String correctMusicTitle = music.getString("musicName");
+
+                            chooseBtn1.setText(correctMusicTitle);
+
                             mPlayer = MediaPlayer.create(getApplicationContext(),getResources().getIdentifier(src, "raw", getPackageName()));
                             mPlayer.start();
-//                            title.setText(name);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
